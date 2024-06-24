1bba927c4fd6082bcb0c4a8027d2b5640667a4c6
Mitch Ryan <mitch@diesdas.digital>
2024-06-24 13:31:48 +0200

    Add script which nukes all ad "listings" on Kleinanzeigen

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,26 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+if (window.location.hostname === "www.kleinanzeigen.de") {
+  const observer = new MutationObserver((mutationList) => {
+    for (const mutation of mutationList) {
+      /**
+       * Deliberately not using querySelectorAll because we only need to remove
+       * one to trigger this mutation effect again, which has the effect of
+       * looping over all children.
+       */
+      const ad = document.querySelector(
+        ".j-liberty-wrapper, .ad-listitem:not(:has(article))"
+      );
+      if (ad) {
+        ad.remove();
+      }
+    }
+  });
+
+  const siteContent = document.querySelector("#site-content");
+  if (siteContent) {
+    observer.observe(siteContent, {
+      attributes: false,
+      childList: true,
+      subtree: true,
+    });
   }
-});
+}

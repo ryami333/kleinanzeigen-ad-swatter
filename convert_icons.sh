7e4de0522c61c71ddaeef1f1f5578d9b84ad58b4
Mitch Ryan <mitch@diesdas.digital>
2024-06-30 14:06:50 +0200

    Use `rsvgconvert` instead of `imagemagick` because it correctly converts paths in the source SVG

--- convert_icons.sh
+++ convert_icons.sh
@@ -5,5 +5,5 @@ sizes=(16 32 48 128)
 
 # Loop through each size and convert the SVG to PNG with the specified size
 for size in ${sizes[@]}; do
-    magick icon.svg -resize ${size}x${size} "dist/icon${size}.png"
+    rsvg-convert -w $size -h $size icon.svg -o "dist/icon${size}.png"
 done
